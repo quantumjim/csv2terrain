4e5667a92bc4e323a2b6f072245c3dc31954d816
James Wootton <decodoku@gmail.com>
2019-05-22 11:18:52 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# csv2terrain
-A mod for Minetest
+## csv2terrain
+
+This is a mod for [Minetest](https://github.com/minetest/). It has been designed to be used in conjunction with the `default` mod of the Minetest game.
+
+This mod reads in data from a local csv file, which should have lines of the form
+
+`x,y,z,keyword,`
+
+Where `x`, `y` and `z` are numbers. 
+
+If the keyword is *player*, the position (x,y,z) is used as the spawn point of the player whenever a new game is started or a file is openened
+
+The keywords can also be names of blocks. The following are known to work (in conjunction with the default mod).
+
+```
+dirt_with_grass, sand, stone, 
+water_source, lava_source, 
+tree, leaves, torch, 
+diamondblock, goldblock, stone_with_coal, stone_with_iron, stone_with_copper, stone_with_tin, stone_with_gold, stone_with_diamond
+fern_1, fern_2, fern_3, marram_grass_1, marram_grass_2, marram_grass_3
+```
+
+The mod adds these blocks to their specified positions whenenever the command `/ltbv` is invoked. This stands for 'Let there be voxels!', to allow the player their moment of creation.
+
+If there are weird gaps in terrain generated in this way, simply try invoking the command again to patch it up. If there are weird dark patches, it seems best to just try again with a new world.
+
+Note that keywords *min* and *max* also need to be present in the csv. The coordinates for these define the volume in which all the blocks reside.
+
+A csv generator is included within the mod, generates terrain using [Qiskit](https://qiskit.org). It needs to be run separately from the game, and runs using Python 3. Simply run the file 'island_generator.py' using a command such as
+
+```
+python3 island_generator.py
+```
---
+++ init.lua
@@ -0,0 +1,60 @@
+-- See the README for information
+
+function csv2terrain ()
+
+	for line in io.lines(minetest.get_modpath("csv2terrain").."/blocks.csv")  do
+		local x, y, z, block = line:match("%s*(.-),%s*(.-),%s*(.-),%s*(.-),")
+		if block=="min" then
+			x0, y0, z0 = tonumber(x), tonumber(y), tonumber(z)
+    	end
+    	if block=="max" then
+       		 x1, y1, z1 = tonumber(x), tonumber(y), tonumber(z)
+   		end
+  	end
+
+	local pos1 = {x = x0, y = y0, z = z0}
+	local pos2 = {x = x1, y = y1, z = z1}
+
+	local manip = minetest.get_voxel_manip()
+	local edge0, edge1 = manip:read_from_map(pos1, pos2)
+ 	local area = VoxelArea:new{MinEdge=edge0, MaxEdge=edge1}
+  	local data = manip:get_data()
+
+  	for line in io.lines(minetest.get_modpath("csv2terrain").."/blocks.csv")  do
+    	local x, y, z, block = line:match("%s*(.-),%s*(.-),%s*(.-),%s*(.-),")
+        local j = area:index(tonumber(x), tonumber(y), tonumber(z))
+   		if block=="air" then
+     	 		data[j] = minetest.get_content_id (block)
+    		else
+      			data[j] = minetest.get_content_id ("default:"..block)
+  		end
+ 	 end
+  
+ 	manip:set_data(data)
+ 	manip:write_to_map()
+	manip:update_map()
+  
+end
+
+minetest.register_chatcommand("ltbv", {
+	func = csv2terrain,
+})
+
+
+minetest.register_on_joinplayer(function(player) 
+    playername = player.get_player_name(player)
+	local privs = minetest.get_player_privs(playername)
+	privs.fly = true
+	minetest.set_player_privs(playername, privs)
+	for line in io.lines(minetest.get_modpath("csv2terrain").."/blocks.csv")  do
+		local x, y, z, block = line:match("%s*(.-),%s*(.-),%s*(.-),%s*(.-),")
+		if block=="player" then
+			xp, yp, zp = tonumber(x), tonumber(y), tonumber(z)
+			player:set_pos( {x=xp, y=yp, z=zp} )
+		end
+  	end        
+end)
+
+
+
+
